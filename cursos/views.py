ebfbf294ef095c49b8cd88d15399872f646452ba
DiogoNunes <diogo2005santosnunes@gmail.com>
2026-04-24 17:50:31 +0000

    ficha 7 done

--- cursos/views.py
+++ cursos/views.py
@@ -1,10 +1,20 @@
 ##  ficheiro escola/views.py
 
 from django.shortcuts import render
-from .models import Curso
+from .models import Curso,Aluno
 
 def cursos_view(request):
     
     cursos = Curso.objects.select_related('professor').prefetch_related('alunos').all()
     
     return render(request, 'cursos/cursos.html', {'cursos': cursos})
+
+def curso_view(request, id):
+
+    curso=Curso.objects.get(id=id)     
+
+    return render(request, 'cursos/curso.html', {'curso': curso})
+
+def alunos_view(request):
+    alunos = Aluno.objects.all()
+    return render(request, "cursos/alunos.html", {"alunos": alunos})
